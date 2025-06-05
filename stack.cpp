fdacd4b045ef5aa259dd2c956a4a0bd6f118fc8d
MUHAMADRAFLIPRATAMA <kipxrap@gmail.com>
2025-06-05 15:35:22 +0700

    4. update the top pointer to the new node

--- stack.cpp
+++ stack.cpp
@@ -32,4 +32,5 @@ class stack
         Node *newNode = new Node();//1. alocate memory for the new node
         newNode->data = value; // 2.assign value
         newNode->next = top; // 3. set the next pointer of the new node to the current top
-       
+        top = newNode; // 4. update the top pointer to the new node
+        
